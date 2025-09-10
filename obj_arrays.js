3ec25d4b871e154a7940400dc5eac08d6b18767e
HABBIE-MARIA-EUGENE <meugenem1409@gmail.com>
2025-09-10 14:45:31 +0530

    added

---
+++ obj_arrays.js
@@ -0,0 +1,10 @@
+// wrkng with objs
+const user = {
+    name: "Habbie maria eugene M",
+    email: "meugenem1409@gmail.com",
+
+    login() {
+        console.log(`${this.name} logged in`);
+    }
+};
+
